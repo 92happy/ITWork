4be19d786f02534155a9b874e9b5f41b7adc77a7
92happy <92happy@gmail.com>
2019-01-05 21:30:47 +0800

    itwork

--- itwork/2/mysql.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$mysqli = new mysqli("localhost","root","xyz789","itwork");
-	if (!$mysqli){
-		printf("Message:",mysql_connect_error());
-		exit;
-		} 
-	$mysqli->query("SET NAMES 'utf8'");
