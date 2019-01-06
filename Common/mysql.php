406697bd54fc6f947248b3c12f8f0797121ff66b
92happy <92happy@gmail.com>
2019-01-06 09:19:14 +0800

    up

--- Common/mysql.php
+++ Common/mysql.php
@@ -1,5 +1,5 @@
 <?php
-	$mysqli = new mysqli("localhost","root","xyz789","itwork");
+	$mysqli = new mysqli("localhost","root","********","itwork");
 	if (!$mysqli){
 		printf("Message:",mysql_connect_error());
 		exit;
